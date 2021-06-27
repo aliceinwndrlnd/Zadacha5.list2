c31603e2c02f28fbfb6a49ac76570a4792f4f3d9
Alesya Yurkevich <lesja_1_gv@mail.ru>
2021-06-27 23:29:55 +0300

    izmenila nazvaniya peremennyx

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int sm = 0;
-	double dujm = 2.54;
+	int dujm = 0;
+	double sm = 2.54;
 	double result = 0;
 	for (int i = 1;i <= 20;i++) {
-	result = i * dujm;
-	sm = sm+1;
-	System.out.println(sm + " см. " + "равен " + result + " д.");
+	result = i * sm;
+	dujm = dujm+1;
+	System.out.println(dujm + " д. " + "равен " + result + " см.");
     }
 
     }
